078af1d95010e3b41cd981afb8f4f2bed14d1567
veetinuutinen-art <veeti.nuutinen@icloud.com>
2026-07-22 17:26:41 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,47 +1,33 @@
 import sqlite3
-
 from datetime import datetime
 
 DB = "tracker.db"
 
+
 def init_db():
-
     conn = sqlite3.connect(DB)
-
     c = conn.cursor()
 
     c.execute("""
-
     CREATE TABLE IF NOT EXISTS alerts (
-
         name TEXT,
-
-        change REAL,
-
         date TEXT
-
     )
-
     """)
 
     conn.commit()
-
     conn.close()
 
-def already_sent(name, change):
 
+def already_sent(name):
     conn = sqlite3.connect(DB)
-
     c = conn.cursor()
 
     today = datetime.now().strftime("%Y-%m-%d")
 
     c.execute(
-
         "SELECT * FROM alerts WHERE name=? AND date=?",
-
         (name, today)
-
     )
 
     result = c.fetchone()
@@ -50,28 +36,18 @@ def already_sent(name, change):
 
     return result is not None
 
-def save_alert(name, change):
 
+def save_alert(name):
     conn = sqlite3.connect(DB)
-
     c = conn.cursor()
 
     c.execute(
-
-        "INSERT INTO alerts VALUES (?, ?, ?)",
-
+        "INSERT INTO alerts VALUES (?, ?)",
         (
-
             name,
-
-            change,
-
             datetime.now().strftime("%Y-%m-%d")
-
         )
-
     )
 
     conn.commit()
-
     conn.close()
