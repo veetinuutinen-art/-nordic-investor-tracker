000ee06b8db13a498feede9428772900c188d1f2
veetinuutinen-art <veeti.nuutinen@icloud.com>
2026-07-22 17:58:07 +0300

    Update insider.py

--- insider.py
+++ insider.py
@@ -1,26 +1,2 @@
-import requests
-
-
 def get_insider_buys():
-
-    url = "https://www.nasdaqomxnordic.com/"
-
-    headers = {
-        "User-Agent": "Mozilla/5.0"
-    }
-
-    try:
-        r = requests.get(
-            url,
-            headers=headers,
-            timeout=20
-        )
-
-        print("INSIDER TEST STATUS:", r.status_code)
-        print(r.text[:300])
-
-        return []
-
-    except Exception as e:
-        print("INSIDER ERROR:", e)
-        return []
+    return []
